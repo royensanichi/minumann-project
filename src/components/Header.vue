ccd6598a5996b231e540a248baf781e17a5a2d41
royensanichi <ravexroyen@gmail.com>
2025-08-13 17:14:44 +0700

    added goals, View, basic components

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="header">
+    <h1>Minuman Yukk Header</h1>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SideBar from '@/components/SideBar.vue'
+import Header from '@/components/Header.vue'
+import Content from '@/components/Content.vue'
+</script>
+
+<template>
+  <div class="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen flex w-screen">
+    <aside>
+      <SideBar />
+    </aside>
+
+    <div class="flex flex-col flex-1">
+      <header>
+        <Header />
+      </header>
+
+      <main class="flex-1">
+        <Content />
+      </main>
+    </div>
+  </div>
+</template>
