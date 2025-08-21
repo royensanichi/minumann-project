a010d2b75f121eaf2891d522ab190cb8f59ce64b
royensanichi <ravexroyen@gmail.com>
2025-08-21 09:21:05 +0700

    added loop to add data menu to Content, all needed is the data for the menu

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -33,97 +33,30 @@ const drinkStore = useDrinkStore()
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-8">
     <div class="rounded">
       <h2 class="text-2xl font-bold tracking-tight text-gray-900">
-        {{ drinkStore.selectedDrink }} Menu
+        "{{ drinkStore.selectedDrink }}" Menu
       </h2>
-
-      <ul>
-        {Tea.map((item) => (
-        <li key="{item.name}">
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          <p>Price: {item.price}</p>
-          <img src="{item.image}" alt="{item.name}" />
-        </li>
-        ))}
-      </ul>
       <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <div class="group relative">
+        <!-- the menu items -->
+        <div v-for="item in Tea" :key="item.name" class="group relative">
           <img
             src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
             alt="Front of men&#039;s Basic Tee in black."
             class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
           />
-          <div class="mt-4 flex justify-between">
+          <div class="mt-2 flex justify-between">
             <div>
               <h3 class="text-sm text-gray-700">
                 <a href="#">
                   <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
+                  {{ item.name }}
                 </a>
               </h3>
-              <p class="mt-1 text-sm text-gray-500">Black</p>
+              <p class="mt-1 text-sm text-gray-500">{{ item.description }}</p>
             </div>
-            <p class="text-sm font-medium text-gray-900">$35</p>
-          </div>
-        </div>
-        <div class="group relative">
-          <img
-            src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg"
-            alt="Front of men&#039;s Basic Tee in white."
-            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-          />
-          <div class="mt-4 flex justify-between">
-            <div>
-              <h3 class="text-sm text-gray-700">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
-                </a>
-              </h3>
-              <p class="mt-1 text-sm text-gray-500">Aspen White</p>
-            </div>
-            <p class="text-sm font-medium text-gray-900">$35</p>
-          </div>
-        </div>
-        <div class="group relative">
-          <img
-            src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg"
-            alt="Front of men&#039;s Basic Tee in dark gray."
-            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-          />
-          <div class="mt-4 flex justify-between">
-            <div>
-              <h3 class="text-sm text-gray-700">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
-                </a>
-              </h3>
-              <p class="mt-1 text-sm text-gray-500">Charcoal</p>
-            </div>
-            <p class="text-sm font-medium text-gray-900">$35</p>
-          </div>
-        </div>
-        <div class="group relative">
-          <img
-            src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
-            alt="Front of men&#039;s Artwork Tee in peach with white and brown dots forming an isometric cube."
-            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-          />
-          <div class="mt-4 flex justify-between">
-            <div>
-              <h3 class="text-sm text-gray-700">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Artwork Tee
-                </a>
-              </h3>
-              <p class="mt-1 text-sm text-gray-500">Iso Dots</p>
-            </div>
-            <p class="text-sm font-medium text-gray-900">$35</p>
+            <p class="text-sm font-medium text-gray-900">Rp. {{ item.price }}</p>
           </div>
         </div>
       </div>
