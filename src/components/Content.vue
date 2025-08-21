874c2170765a12898e2d75cd546ee2ee0ce292e0
royensanichi <ravexroyen@gmail.com>
2025-08-21 11:38:32 +0700

    added json, update on menu picture on public image, re edit header for banner

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,50 +1,41 @@
 <script setup lang="ts">
 import { useDrinkStore } from '@/stores/drinkStore'
-
-const Tea = [
-  {
-    name: 'S-Tea',
-    description: 'A soothing beverage made from the leaves of the Camellia sinensis plant.',
-    price: 8000,
-    image: 'https://example.com/tea.jpg',
-  },
-  {
-    name: 'Herbal Tea',
-    description: 'A caffeine-free infusion made from a variety of herbs and spices.',
-    price: 38000,
-    image: 'https://example.com/herbal-tea.jpg',
-  },
-  {
-    name: 'Green Tea',
-    description: 'A refreshing beverage made from unoxidized tea leaves.',
-    price: 9000,
-    image: 'https://example.com/green-tea.jpg',
-  },
-  {
-    name: 'Mango-Green Tea',
-    description: 'A chilled beverage made from brewed tea, served over ice.',
-    price: 45000,
-    image: 'https://example.com/iced-tea.jpg',
-  },
-]
+import { Tea, Coffee, OurSignature, Juice } from '@/components/Product.json'
+import { computed } from 'vue'
 
 // Access store
 const drinkStore = useDrinkStore()
+
+const productsToShow = computed(() => {
+  switch (drinkStore.selectedDrink) {
+    case 'Tea':
+      return Tea
+    case 'Coffee':
+      return Coffee
+    case 'Our Signature':
+      return OurSignature
+    case 'Juice':
+      return Juice
+    default:
+      return []
+  }
+})
 </script>
 
 <template>
-  <div class="p-8">
+  <div class="p-12">
     <div class="rounded">
-      <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900">
         "{{ drinkStore.selectedDrink }}" Menu
       </h2>
+
       <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         <!-- the menu items -->
-        <div v-for="item in Tea" :key="item.name" class="group relative">
+        <div v-for="item in productsToShow" :key="item.name" class="group relative bg-gray-800">
           <img
-            src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
-            alt="Front of men&#039;s Basic Tee in black."
-            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            :src="item.image"
+            alt="Front of men's Basic Tee in black."
+            class="aspect-square w-full rounded-md bg-gray-200 object-contain group-hover:opacity-75 lg:aspect-auto lg:h-90"
           />
           <div class="mt-2 flex justify-between">
             <div>
@@ -56,7 +47,7 @@ const drinkStore = useDrinkStore()
               </h3>
               <p class="mt-1 text-sm text-gray-500">{{ item.description }}</p>
             </div>
-            <p class="text-sm font-medium text-gray-900">Rp. {{ item.price }}</p>
+            <p class="text-sm font-medium text-black-900">Rp. {{ item.price }}</p>
           </div>
         </div>
       </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,9 @@
   <header class="p-1 flex flex-row items-center justify-center shadow-md gap-4">
     <img src="../assets/icons/minumanyokk_logo.png" alt="Logo" class="w-24 h-24 p-1" />
     <h1 class="text-2xl font-bold text-white w-full text-center">OUR MENU</h1>
-    <button class="bg-red-800 hover:bg-red-600 transition p-1 text-white rounded-md h-12 text-lg">
+    <img src="/img/banner/MinumanYukkBanner.webp" />
+    <!-- <button class="bg-red-800 hover:bg-red-600 transition p-1 text-white rounded-md h-12 text-lg">
       Keranjang
-    </button>
+    </button> -->
   </header>
 </template>
